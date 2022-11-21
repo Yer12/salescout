50cf40294cbc05078a58172e9a8b719366f1c1a5
Yer12 <gynatik3@gmail.com>
2022-11-22 00:50:15 +0600

    fix

---
+++ script.js
@@ -0,0 +1,26 @@
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    spaceBetween: 10,
+    pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+var swiper = new Swiper(".mySwiper1", {
+    slidesPerView: "auto",
+    spaceBetween: 10,
+    pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
