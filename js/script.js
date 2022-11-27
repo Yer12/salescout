74b7c111296bc46a76b17ff730c183ff7a4378cd
erlan.u <erlan.u@1fit.app>
2022-11-27 17:35:02 +0600

    image modal

--- js/script.js
+++ js/script.js
@@ -151,3 +151,30 @@ for (i = 0; i < acc.length; i++) {
         }
     });
 }
+
+// Modal Setup
+var modal = document.getElementById('modal');
+
+var modalClose = document.getElementById('modal-close');
+modalClose.addEventListener('click', function() {
+    modal.style.display = "none";
+});
+
+// global handler
+document.addEventListener('click', function (e) {
+    if (e.target.className.indexOf('modal-target') !== -1) {
+        var img = e.target;
+        var modalImg = document.getElementById("modal-content");
+        var captionText = document.getElementById("modal-caption");
+        if(mobile.matches) {
+            modal.style.display = "flex";
+        }
+        else {
+            modal.style.display = "block";
+        }
+        modalImg.src = img.src;
+        captionText.innerHTML = img.alt;
+    }
+    document.body.style.overflow = document.body.style.overflow ? null : 'hidden';
+
+});
